1f4ddcdc24531aa1038057a5f702a1352cc6c450
MTrimm <123900929+MTrimm@users.noreply.github.com>
2023-09-12 16:03:21 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//Mathew Trimm
+//mtrimm@stumail.northeaststate.edu
+
+
+
+namespace Lab04
+
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float velocity0 = 5.6f;
+            float velocity1 = 10.5f;
+            float time = 0.5f;
+
+            float acceleration = (velocity1 - velocity0) / time;
+
+            Console.WriteLine("acceleration: " + acceleration);
+
+
+
+
+        }
+    }
+}
